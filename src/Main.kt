41ed444ac7d6a92e3afca65e04208c67e285aa57
Nicholas Rehac <tup88289@temple.edu>
2024-01-23 12:11:27 -0500

    added tests in main

--- src/Main.kt
+++ src/Main.kt
@@ -1,2 +1,7 @@
-class Main {
+fun main() {
+    val thing = Temperature(TemperatureUnit.Fahrenheit)
+    thing.setTemperature(25.0)
+    println(thing.getTemperatureInC())
+    println(thing.getTemperatureInF())
+    println(thing.getTemperatureInK())
 }
